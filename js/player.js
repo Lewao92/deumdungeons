e6a1234416f8084958f63339b0e2360ab3b1c099
Lewao92 <lewinssonrichard@gmail.com>
2021-06-19 21:24:47 +0200

    0.002
    Update igen

--- js/player.js
+++ js/player.js
@@ -55,7 +55,7 @@ let PlayerMoves = {
             if (enemy.hälsa <= 0) {
                 alert("Du vann! Du slog i hjäl monstret! Uppdatera sidan för att spela igen.");
                 getPlayerHälsa.innerHTML = 'Hälsa:  ' + player.hälsa;
-                getPlayerHälsa.innerHTML = 'Hälsa:  0';
+                getPlayerHälsa.innerHTML = 'Hälsa: 0';
             } else {
                 getEnemyHälsa.innerHTML = 'Hälsa:  ' + enemy.hälsa;
                 // Enemy attacks
@@ -65,10 +65,36 @@ let PlayerMoves = {
                 alert("Monstret hit " + enemyAttackValues[0] + " damage " + enemyAttackValues[1] + " times.");
                 if (player.hälsa <= 0) {
                     alert("Du förlorade och avlider... Uppdatera sidan  för att spela igen.");
-                    getPlayerHälsa.innerHTML = 'Hälsa:  0';
+                    getPlayerHälsa.innerHTML = 'Hälsa: 0';
                     getEnemyHälsa.innerHTML = 'Hälsa:  ' + enemy.hälsa;
                 } else {
                     getPlayerHälsa.innerHTML = 'Hälsa:  ' + player.hälsa;
+                }
+            }
+        }
+        // Enemy attacks
+        else if (getEnemyUthållighet >= getPlayerUthållighet) {
+            let enemyAttackValues = enemyAttack();
+            let totalDamage = enemyAttackValues[0] * enemyAttackValues[1];
+            player.hälsa = player.hälsa - totalDamage;
+            alert("Monstret slår " + enemyAttackValues[0] + " damage " + enemyAttackValues[1] + " times.");
+            if (player.hälsa <= 0) {
+                alert("Du förlorade och avlider... Uppdatera sidan  för att spela igen.");
+                getEnemyHälsa.innerHTML = 'Hälsa:  ' + enemy.hälsa;
+                getPlayerHälsa.innerHTML = 'Hälsa: 0';
+            } else {
+                getPlayerHälsa.innerHTML = 'Hälsa:  ' + player.hälsa;
+                // player attacks
+                let playerAttackValues = playerAttack();
+                let totalDamage = playerAttackValues[0] * playerAttackValues[1];
+                enemy.hälsa = enemy.hälsa - totalDamage;
+                alert("Du slår " + enemyAttackValues[0] + " damage " + enemyAttackValues[1] + " times.");
+                if (enemy.hälsa <= 0) {
+                    alert("Du vann! Du slog i hjäl monstret! Uppdatera sidan för att spela igen.");
+                    getEnemyHälsa.innerHTML = 'Hälsa: 0';
+                    getPlayerHälsa.innerHTML = 'Hälsa:  ' + enemy.hälsa;
+                } else {
+                    getEnemyHälsa.innerHTML = 'Hälsa:  ' + enemy.hälsa;
                 }
             }
         }
